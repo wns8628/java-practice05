2dcc66f97d1257f02d47c7513c7136e3d857a785
kicks <kickscar@gmail.com>
2019-01-03 15:08:05 +0900

    init commit

---
+++ src/prob5/MyStack.java
@@ -0,0 +1,20 @@
+package prob5;
+
+public class MyStack {
+	private int top;
+	private String[] buffer;
+
+	@SuppressWarnings("unchecked")
+	public MyStack(int capacity) {
+	}
+
+	public void push(String s) {
+	}
+
+	public String pop() throws MyStackException {
+	}
+
+	public boolean isEmpty() {
+		return top == -1;
+	}
+}
